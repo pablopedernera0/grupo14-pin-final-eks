b1329e7d90270c7fda9715ec534c942890f2fa75
Pablo <pablopedernera0@gmail.com>
2025-02-28 20:21:37 -0300

    fix policy

--- iam.tf
+++ iam.tf
@@ -21,7 +21,12 @@ resource "aws_iam_role" "eks_cluster" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServiceRolePolicy"
-  role       = aws_iam_role.eks_cluster.name
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+resource "aws_iam_role_policy_attachment" "vpc_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
