a9cc0bc48893a0f4939548dd602c31036ee6c2a8
Pablo <pablopedernera0@gmail.com>
2025-02-28 20:28:17 -0300

    fix policy

--- iam.tf
+++ iam.tf
@@ -2,31 +2,31 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_iam_role" "eks_cluster" {
-  name        = "eks-cluster-role"
-  description = "EKS Cluster Role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Effect = "Allow",
         Principal = {
           Service = "eks.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid      = ""
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
-resource "aws_iam_role_policy_attachment" "vpc_policy_attachment" {
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
+
 
