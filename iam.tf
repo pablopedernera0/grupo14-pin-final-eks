2b53e43323106d25a2a4a703aa7443d94a7eee6a
desarrollo <pgpedernera@santafe.gov.ar>
2025-02-28 09:44:36 -0300

    fix role

--- iam.tf
+++ iam.tf
@@ -1,3 +1,27 @@
 provider "aws" {
   region = "sa-east-1"
 }
+
+resource "aws_iam_role" "eks_cluster" {
+  name        = "eks-cluster-role"
+  description = "EKS Cluster Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid      = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServiceRolePolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
